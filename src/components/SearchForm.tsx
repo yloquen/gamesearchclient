427edecbc71f8fd559e2464a07268bd9faa8129f
Ivan Kostov <i.kostov@wienergames.com>
2022-01-18 17:45:47 +0200

    progress

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -5,9 +5,17 @@ import {LegacyRef} from "react";
 
 export function SearchForm(props:any)
 {
+    const logoStyle =
+    {
+        position:"absolute",
+        left:"1.5vw"
+    };
+
     const ref:any = React.createRef();
     return (
         <div id="search_form_container">
+            <img style={logoStyle} alt="" src="./assets/game_search_logo.png"/>
+
             <input id="search_form_input" type="text" ref={ref}/>
             <button id="search_form_button" onClick={() => {app.controller.runQuery(ref.current.value)}}>
                 Search
